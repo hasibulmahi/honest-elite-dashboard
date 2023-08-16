4f933a752524ef10763daf46275d644a40fe9036
Hasibul Mahi <hasibulmahi@gmail.com>
2023-08-17 01:43:08 +0600

    Update sidebar-item.js

--- components/sidebar/sidebar-item.js
+++ components/sidebar/sidebar-item.js
@@ -1,34 +1,34 @@
 export const CategoryItem = [
-    {
-        name: "Content Type",
-        icon: "</>",
-    },
-    {
-        name: "Category List",
-        icon: "</>",
-    },
-    {
-        name: "SUb Category List",
-        icon: "</>",
-    },
-    {
-        name: "Menu Category",
-        icon: "</>",
-    },
-    {
-        name: "Model Category",
-        icon: "</>",
-    },
-    {
-        name: "Side Bar Item 01",
-        icon: "</>",
-    },
-    {
-        name: "Side Bar Item 02",
-        icon: "</>",
-    },
-    {
-        name: "Side Bar Item 03",
-        icon: "</>",
-    },
-]
+  {
+    name: "Content Type",
+    icon: "</>",
+  },
+  {
+    name: "Category List",
+    icon: "</>",
+  },
+  {
+    name: "SUb Category List",
+    icon: "</>",
+  },
+  {
+    name: "Menu Category",
+    icon: "</>",
+  },
+  {
+    name: "Model Category",
+    icon: "</>",
+  },
+  {
+    name: "Side Bar Item 01",
+    icon: "</>",
+  },
+  {
+    name: "Side Bar Item 02",
+    icon: "</>",
+  },
+  {
+    name: "Side Bar Item 03",
+    icon: "</>",
+  },
+];
