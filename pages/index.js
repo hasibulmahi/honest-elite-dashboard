58ea3d7839ce24773e3853cdf99f0786ba84112a
Hasibul Mahi <hasibulmahi@gmail.com>
2023-08-18 23:55:19 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/sidebar/sidebar.component";
 import Navigation from "@/components/navigation/navigation.component";
+import DataTables from "@/components/table/table.component";
+import Layout from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <Sidebar />
-      <Navigation/>
-    </>
+    <Layout>
+      <DataTables />
+    </Layout>
   );
 }
