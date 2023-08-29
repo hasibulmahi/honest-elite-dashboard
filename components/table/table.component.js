5350910eb4fb9462fcd99a2b8cd165720a166a48
Hasibul Mahi <hasibulmahi@gmail.com>
2023-08-30 03:08:09 +0600

    Update table.component.js

--- components/table/table.component.js
+++ components/table/table.component.js
@@ -63,7 +63,7 @@ function DataTables(props) {
 
   const data = [
     { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
-    { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+    { name: "Hasibul Mahi", company: "Test Corp", city: "Hartford", state: "CT" },
     { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
     {
       name: "James Houston",
