2ced453350413db0156ed425ee2dfa6d14939a3b
Hasibul Mahi <hasibulmahi@gmail.com>
2023-09-01 02:42:46 +0600

    Update navigation.component.js

--- components/navigation/navigation.component.js
+++ components/navigation/navigation.component.js
@@ -93,7 +93,7 @@ function Navigation(props) {
           </IconButton>
           <Tooltip title="Open settings">
             <IconButton>
-              <Avatar alt="Hilary Clinton" src="/profile.png" />
+              <Avatar alt="Barack Obama" src="/profile.png" />
             </IconButton>
           </Tooltip>
         </Box>
